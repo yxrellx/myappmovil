245c4530e25b9774698d3c3088e07b93989d959f
yxrellx <yarella.rojas02@inacapmail.cl>
2022-10-24 22:40:27 -0300

    xhj

--- app/src/main/java/com/example/myappmovil/MainActivity.java
+++ app/src/main/java/com/example/myappmovil/MainActivity.java
@@ -1,18 +1,57 @@
-package com.example.myappmovil;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnregistrar;
+    EditText etNombre, etApellido, etEdad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        llamadoBtnregistrar();
+        llamadoBD();
+        etNombre = (EditText) findViewById(R.id.etNombre);
+        etApellido = (EditText) findViewById(R.id.etApellido);
+        etEdad = (EditText) findViewById(R.id.etEdad);
     }
 
+    private void llamadoBD() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Alumno").child("r1");
+        myRef.child("nombre").setValue("Javier");
+        myRef.child("apellido").setValue("Villa");
+        myRef.child("edad").setValue("18");
+    }
 
-    
+    public void llamadoBtnregistrar(){
+        btnregistrar = (Button) findViewById(R.id.btnregistrar);
+        btnregistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre = etNombre.getText().toString();
+                String apellido = etApellido.getText().toString();
+                String edad = etEdad.getText().toString();
+                String key = UUID.randomUUID().toString();
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("Alumno").child(key);
+                myRef.child("nombre").setValue(nombre);
+                myRef.child("apellido").setValue(apellido);
+                myRef.child("edad").setValue(edad);
+            }
+        });
+    }
 }
-
